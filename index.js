2d53ea28e41c4880159842268b762642f4779f32
Hickacou <hello@hicka.dev>
2021-04-24 13:26:28 +0200

    fix: remove express-fileupload usage (198507c)

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 require('dotenv').config();
 const fs = require('fs');
 const express = require('express');
-const fileupload = require('express-fileupload');
 const app = express();
 
 if (!fs.existsSync('logs'))
 	fs.mkdirSync('logs');
 
 app.use(express.json());
-app.use(fileupload({
-	limits: {
-		fileSize: 2 * 1024 * 1024
-	},
-	limitHandler: function (_req, res) {
-		res.status(400).json({ success: false, error: 'File is too big. Must be 2MB or lower.' });
-	}
-}));
 app.use(require('./middleware/log'));
 app.use('/color', require('./routes/color'));
 
